6edb000cd2ded59731f7b32cb1b9600bd098b215
Yury Molodov <yurymolodov@gmail.com>
2025-12-10 14:00:29 +0100

    app/vmui: remove fill option from hits chart

--- app/vmui/packages/vmui/src/components/Chart/BarHitsChart/BarHitsOptions/BarHitsOptions.tsx
+++ app/vmui/packages/vmui/src/components/Chart/BarHitsChart/BarHitsOptions/BarHitsOptions.tsx
@@ -1,14 +1,12 @@
-import { FC, useEffect, useMemo, useRef } from "preact/compat";
+import { FC, useEffect, useMemo } from "preact/compat";
 import { GraphOptions, GRAPH_STYLES } from "../types";
 import Switch from "../../../Main/Switch/Switch";
 import "./style.scss";
 import useStateSearchParams from "../../../../hooks/useStateSearchParams";
 import { useSearchParams } from "react-router-dom";
 import Button from "../../../Main/Button/Button";
-import { SettingsIcon, TipIcon, VisibilityIcon, VisibilityOffIcon } from "../../../Main/Icons";
+import { TipIcon, VisibilityIcon, VisibilityOffIcon } from "../../../Main/Icons";
 import Tooltip from "../../../Main/Tooltip/Tooltip";
-import Popper from "../../../Main/Popper/Popper";
-import useBoolean from "../../../../hooks/useBoolean";
 import ShortcutKeys from "../../../Main/ShortcutKeys/ShortcutKeys";
 
 interface Props {
@@ -17,29 +15,16 @@ interface Props {
 
 const BarHitsOptions: FC<Props> = ({ onChange }) => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const optionsButtonRef = useRef<HTMLDivElement>(null);
-  const {
-    value: openOptions,
-    toggle: toggleOpenOptions,
-    setFalse: handleCloseOptions,
-  } = useBoolean(false);
 
   const [stacked, setStacked] = useStateSearchParams(false, "stacked");
-  const [fill, setFill] = useStateSearchParams("true", "fill");
   const [hideChart, setHideChart] = useStateSearchParams(false, "hide_chart");
 
   const options: GraphOptions = useMemo(() => ({
     graphStyle: GRAPH_STYLES.BAR,
     stacked,
-    fill: fill === "true",
+    fill: true,
     hideChart,
-  }), [stacked, fill, hideChart]);
-
-  const handleChangeFill = (val: boolean) => {
-    setFill(`${val}`);
-    searchParams.set("fill", `${val}`);
-    setSearchParams(searchParams);
-  };
+  }), [stacked, hideChart]);
 
   const handleChangeStacked = (val: boolean) => {
     setStacked(val);
@@ -62,6 +47,13 @@ const BarHitsOptions: FC<Props> = ({ onChange }) => {
 
   return (
     <div className="vm-bar-hits-options">
+      <div className="vm-bar-hits-options-item">
+        <Switch
+          label={"Stacked"}
+          value={stacked}
+          onChange={handleChangeStacked}
+        />
+      </div>
       <ShortcutKeys>
         <Button
           variant="text"
@@ -69,17 +61,6 @@ const BarHitsOptions: FC<Props> = ({ onChange }) => {
           startIcon={<TipIcon/>}
         />
       </ShortcutKeys>
-      <div ref={optionsButtonRef}>
-        <Tooltip title="Graph settings">
-          <Button
-            variant="text"
-            color="primary"
-            startIcon={<SettingsIcon/>}
-            onClick={toggleOpenOptions}
-            ariaLabel="settings"
-          />
-        </Tooltip>
-      </div>
       <Tooltip title={hideChart ? "Show chart and resume hits updates" : "Hide chart and pause hits updates"}>
         <Button
           variant="text"
@@ -89,30 +70,6 @@ const BarHitsOptions: FC<Props> = ({ onChange }) => {
           ariaLabel="settings"
         />
       </Tooltip>
-      <Popper
-        open={openOptions}
-        placement="bottom-right"
-        onClose={handleCloseOptions}
-        buttonRef={optionsButtonRef}
-        title={"Graph settings"}
-      >
-        <div className="vm-bar-hits-options-settings">
-          <div className="vm-bar-hits-options-settings-item">
-            <Switch
-              label={"Stacked"}
-              value={stacked}
-              onChange={handleChangeStacked}
-            />
-          </div>
-          <div className="vm-bar-hits-options-settings-item">
-            <Switch
-              label={"Fill"}
-              value={fill === "true"}
-              onChange={handleChangeFill}
-            />
-          </div>
-        </div>
-      </Popper>
     </div>
   );
 };
