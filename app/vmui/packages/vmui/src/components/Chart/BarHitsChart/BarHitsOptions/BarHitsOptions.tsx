05e293d4d98714279bd9acc7ce0154909a963853
Yury Molodov <yurymolodov@gmail.com>
2026-02-16 16:20:41 +0100

    app/vmui: rework Table View
    - add column resizing and reordering
    - fix Table View timestamps for selected timezone
    - fix Table View copy action to copy only selected columns

--- app/vmui/packages/vmui/src/components/Chart/BarHitsChart/BarHitsOptions/BarHitsOptions.tsx
+++ app/vmui/packages/vmui/src/components/Chart/BarHitsChart/BarHitsOptions/BarHitsOptions.tsx
@@ -168,11 +168,12 @@ const BarHitsOptions: FC<Props> = ({ query, isHitsMode, isOverview, onChange }) 
   return (
     <div
       className={classNames({
-      "vm-bar-hits-options": true,
-      "vm-bar-hits-options_mobile": isMobile,
-    })}
+        "vm-bar-hits-options": true,
+        "vm-bar-hits-options_mobile": isMobile,
+        "vm-bar-hits-options_hidden": hideChart,
+      })}
     >
-      {!isMobile && (
+      {!isMobile && !hideChart && (
         <>
           <Controls/>
           <ShortcutKeys withHotkey={false}>
@@ -184,14 +185,22 @@ const BarHitsOptions: FC<Props> = ({ query, isHitsMode, isOverview, onChange }) 
           </ShortcutKeys>
         </>
       )}
+      {hideChart && (
+        <div className="vm-bar-hits-options__hidden-info">
+          Hits chart is hidden. Data updates are paused.
+        </div>
+      )}
+
       <Tooltip title={hideChart ? "Show chart and resume hits updates" : "Hide chart and pause hits updates"}>
         <Button
           variant="text"
           color="primary"
-          startIcon={hideChart ? <VisibilityOffIcon/> : <VisibilityIcon/>}
+          startIcon={hideChart ? <VisibilityIcon/> : <VisibilityOffIcon/>}
           onClick={toggleHideChart}
           ariaLabel="settings"
-        />
+        >
+          {hideChart ? "Show chart" : ""}
+        </Button>
       </Tooltip>
 
       {isMobile && (
---
+++ app/vmui/packages/vmui/src/components/Table/TableSettings/TableSettingsDrawer.tsx
@@ -0,0 +1,103 @@
+import { FC, useState, useRef, useCallback } from "preact/compat";
+import { CloseIcon } from "../../Main/Icons";
+import "./style.scss";
+import Button from "../../Main/Button/Button";
+import useEventListener from "../../../hooks/useEventListener";
+import { useLocation, useNavigate } from "react-router-dom";
+import DragResizeHandle from "../../Main/DragResizeHandle/DragResizeHandle";
+import { Size, useResizeObserver } from "../../../hooks/useResizeObserver";
+import { TableSettingsProps } from "./TableSettings";
+import TableSettingsColumns from "./TableSettingsColumns";
+import { getFromStorage, saveToStorage } from "../../../utils/storage";
+
+interface Props extends TableSettingsProps {
+  title: string;
+  onClose: () => void
+}
+
+const WIDTH_STORAGE_KEY = "LOGS_TABLE_DRAWER";
+
+const getDefaultWidth = () => {
+  const widthFromStorage = getFromStorage(WIDTH_STORAGE_KEY);
+  return widthFromStorage ? Number(widthFromStorage) : 0;
+};
+
+const TableSettingsDrawer: FC<Props> = ({
+  title,
+  columnKeys,
+  viewColumnKeys,
+  statsByKey,
+  dispatchViewColumns,
+  onClose
+}) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [containerWidth, setContainerWidth] = useState(getDefaultWidth());
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState<Size>({
+    width: containerRef?.current?.offsetWidth,
+    height: containerRef?.current?.offsetHeight
+  });
+  useResizeObserver({ ref: containerRef, onResize: setSize });
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    const esc = e.key === "Escape";
+
+    if (esc) {
+      onClose();
+      return;
+    }
+  };
+
+  const handleResizeWidth = (width: number) => {
+    setContainerWidth(width);
+    saveToStorage(WIDTH_STORAGE_KEY, String(width));
+  };
+
+  const handlePopstate = useCallback(() => {
+    navigate(location, { replace: true });
+    onClose();
+  }, [navigate, location]);
+
+  useEventListener("popstate", handlePopstate);
+  useEventListener("keydown", handleKeyDown);
+
+  return (
+    <div className="vm-table-settings-wrapper">
+      <div
+        className="vm-table-settings"
+        style={containerWidth ? { width: `${containerWidth}px` } : undefined}
+        ref={containerRef}
+      >
+        <div className="vm-table-settings-header">
+          <div className="vm-table-settings-header__title">{title}</div>
+
+          <Button
+            variant="text"
+            onClick={onClose}
+            startIcon={<CloseIcon/>}
+          />
+        </div>
+
+        <TableSettingsColumns
+          columnKeys={columnKeys}
+          viewColumnKeys={viewColumnKeys}
+          statsByKey={statsByKey}
+          dispatchViewColumns={dispatchViewColumns}
+        />
+      </div>
+
+      <DragResizeHandle
+        targetRef={containerRef}
+        minSize={250}
+        dir={-1}
+        size={size}
+        onResizeEnd={handleResizeWidth}
+      />
+    </div>
+  );
+};
+
+export default TableSettingsDrawer;
