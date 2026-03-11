VictoriaMetrics/VictoriaLogs

commit 885f548f3f8de4bc5dc20c8e188b5a908bcf5d81

app/vlselect: support for returning query results in CSV format from /select/logsql/query endpoint
VictoriaLogs can return CSV results for queries returning a fixed set of fields.
These are queries ending with `fields` or `stats` pipes.

Other queries can return arbitrary number of fields per each log entry, so it is impossible
to detect the full set of fields across all the returned logs.

Updates https://github.com/VictoriaMetrics/VictoriaLogs/issues/932

app/vlstorage/lastnoptimization.go
@@ -168,16 +168,30 @@ func getLogRowsFromDataBlock(db *logstorage.DataBlock) ([]logRow, error) {
 	columns := db.GetColumns(false)
 
 	columnNames := make([]string, len(columns))
+	var timestampsColumn logstorage.BlockColumn
 	for i, c := range columns {
+		if c.Name == "_time" {
+			timestampsColumn = c
+		}
 		columnNames[i] = strings.Clone(c.Name)
 	}
 
 	lrs := make([]logRow, 0, len(timestamps))
-	fieldsBuf := make([]logstorage.Field, 0, len(columnNames)*len(timestamps))
+	fieldsBuf := make([]logstorage.Field, 0, len(columns)*len(timestamps))
 
 	for i, timestamp := range timestamps {
 		fieldsBufLen := len(fieldsBuf)
+
+		// The _time column must go first, since the query results are sorted by _time.
+		fieldsBuf = append(fieldsBuf, logstorage.Field{
+			Name:  "_time",
+			Value: timestampsColumn.Values[i],
+		})
+
 		for j, c := range columns {
+			if c.Name == "_time" {
+				continue
+			}
 			fieldsBuf = append(fieldsBuf, logstorage.Field{
 				Name:  columnNames[j],
 				Value: strings.Clone(c.Values[i]),

app/vlselect/logsql/csv.go
@@ -1,0 +1,52 @@
+package logsql
+
+import (
+	"strings"
+)
+
+func appendCSVLine(dst []byte, fields []string) []byte {
+	for i := range fields {
+		dst = appendCSVField(dst, fields[i])
+		if i != len(fields)-1 {
+			dst = append(dst, ',')
+		}
+	}
+	dst = append(dst, '\n')
+	return dst
+}
+
+func appendCSVField(dst []byte, s string) []byte {
+	n := strings.IndexAny(s, `",`+"\n")
+	if n < 0 {
+		// fast path - nothing to quote
+		return append(dst, s...)
+	}
+
+	// slow path - the s must be quoted
+	dst = append(dst, '"')
+
+	for {
+		dst = append(dst, s[:n]...)
+
+		ch := s[n]
+		if ch == '"' {
+			dst = append(dst, `""`...)
+		} else {
+			dst = append(dst, ch)
+		}
+
+		s = s[n+1:]
+		if len(s) == 0 {
+			break
+		}
+
+		n := strings.IndexByte(s, '"')
+		if n < 0 {
+			dst = append(dst, s...)
+			break
+		}
+	}
+
+	dst = append(dst, '"')
+	return dst
+}

app/vlselect/logsql/csv_test.go
@@ -1,0 +1,44 @@
+package logsql
+
+import (
+	"testing"
+)
+
+func TestAppendCSVField(t *testing.T) {
+	f := func(s, resultExpected string) {
+		t.Helper()
+
+		result := appendCSVField(nil, s)
+		if string(result) != resultExpected {
+			t.Fatalf("unexpected result\ngot\n%s\nwant\n%s", result, resultExpected)
+		}
+	}
+
+	f("", "")
+	f(" ", " ")
+	f(`"`, `""""`)
+	f(`,`, `","`)
+	f("\n", "\"\n\"")
+	f(`\"`, `"\"""`)
+	f(`\n`, `\n`)
+	f("\r", "\r")
+	f("\t", "\t")
+
+	f(` foo, bar" baz`, `" foo, bar"" baz"`)
+}
+
+func TestAppendCSVLine(t *testing.T) {
+	f := func(fields []string, resultExpected string) {
+		t.Helper()
+
+		result := appendCSVLine(nil, fields)
+		if string(result) != resultExpected {
+			t.Fatalf("unexpected result\ngot\n%s\nwant\n%s", result, resultExpected)
+		}
+	}
+
+	f(nil, "\n")
+	f([]string{"foo"}, "foo\n")
+	f([]string{"a", "", "b"}, "a,,b\n")
+	f([]string{"a,b", `"cd"`, "a\nb,c\"d"}, `"a,b","""cd""","a`+"\n"+`b,c""d"`+"\n")
+}

app/vlselect/logsql/logsql.go
@@ -26,6 +26,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/timeutil"
 	"github.com/VictoriaMetrics/metrics"
 	"github.com/valyala/fastjson"
+	"github.com/valyala/quicktemplate"
 
 	"github.com/VictoriaMetrics/VictoriaLogs/app/vlstorage"
 	"github.com/VictoriaMetrics/VictoriaLogs/lib/logstorage"
@@ -1171,20 +1172,11 @@ func ProcessQueryRequest(ctx context.Context, w http.ResponseWriter, r *http.Req
 		return
 	}
 
-	sw := &syncWriter{
-		w: w,
+	format := r.FormValue("format")
+	if format != "" && format != "csv" {
+		httpserver.Errorf(w, r, "unexpected format=%q; expecting 'csv' or ''", format)
+		return
 	}
-
-	var bwShards atomicutil.Slice[bufferedWriter]
-	bwShards.Init = func(shard *bufferedWriter) {
-		shard.sw = sw
-	}
-	defer func() {
-		shards := bwShards.All()
-		for _, shard := range shards {
-			shard.FlushIgnoreErrors()
-		}
-	}()
 
 	if limit > 0 {
 		// Add '| sort by (_time) desc | offset <offset> | limit <limit>' to the end of the query.
@@ -1195,16 +1187,59 @@ func ProcessQueryRequest(ctx context.Context, w http.ResponseWriter, r *http.Req
 		ca.q.AddPipeOffsetLimit(uint64(offset), uint64(limit))
 	}
 
+	sw := &syncWriter{
+		w: w,
+	}
+
+	var bwShards atomicutil.Slice[bytesutil.ByteBuffer]
+	defer func() {
+		shards := bwShards.All()
+		for _, shard := range shards {
+			if len(shard.B) > 0 {
+				_, _ = sw.Write(shard.B)
+			}
+		}
+	}()
+
+	var csvHeader []byte
+	if format == "csv" {
+		fields, err := ca.q.GetFixedFields()
+		if err != nil {
+			httpserver.Errorf(w, r, "%s", err)
+			return
+		}
+		csvHeader = appendCSVLine(nil, fields)
+	}
+
 	startTime := time.Now()
 	writeResponseHeadersOnce := sync.OnceFunc(func() {
 		// Write response headers
 		h := w.Header()
 
-		h.Set("Content-Type", "application/stream+json")
+		if format == "csv" {
+			h.Set("Content-Type", "text/csv")
+		} else {
+			h.Set("Content-Type", "application/stream+json")
+		}
 		ca.writeResponseHeaders(h, startTime)
+
+		if format == "csv" {
+			_, _ = sw.Write(csvHeader)
+		}
 	})
 
+	var appendRow func(dst []byte, columns []logstorage.BlockColumn, rowIdx int) []byte
+
 	needSortFields := !ca.q.IsFixedOutputFieldsOrder()
+	if format == "csv" {
+		if needSortFields {
+			logger.Panicf("BUG: neetSortFields must be false for format=csv")
+		}
+		appendRow = appendCSVRow
+	} else {
+		appendRow = appendJSONRow
+	}
+
 	writeBlock := func(workerID uint, db *logstorage.DataBlock) {
 		writeResponseHeadersOnce()
 		rowsCount := db.RowsCount()
@@ -1216,9 +1251,10 @@ func ProcessQueryRequest(ctx context.Context, w http.ResponseWriter, r *http.Req
 
 		bw := bwShards.Get(workerID)
 		for i := range rowsCount {
-			WriteJSONRow(bw, columns, i)
-			if len(bw.buf) > 16*1024 {
-				bw.FlushIgnoreErrors()
+			bw.B = appendRow(bw.B, columns, i)
+			if len(bw.B) > 16*1024 {
+				_, _ = sw.Write(bw.B)
+				bw.B = bw.B[:0]
 			}
 		}
 	}
@@ -1234,6 +1270,48 @@ func ProcessQueryRequest(ctx context.Context, w http.ResponseWriter, r *http.Req
 
 	// This call is needed for the case when the response didn't return any results.
 	writeResponseHeadersOnce()
+}
+
+func appendCSVRow(dst []byte, columns []logstorage.BlockColumn, rowIdx int) []byte {
+	for i := range columns {
+		v := columns[i].Values[rowIdx]
+		dst = appendCSVField(dst, v)
+		if i+1 < len(columns) {
+			dst = append(dst, ',')
+		}
+	}
+	dst = append(dst, '\n')
+	return dst
+}
+
+func appendJSONRow(dst []byte, columns []logstorage.BlockColumn, rowIdx int) []byte {
+	dstLen := len(dst)
+	dst = append(dst, '{')
+	for i := range columns {
+		c := &columns[i]
+
+		name := c.Name
+		value := c.Values[rowIdx]
+		if value == "" {
+			// skip empty fields, since they equal to non-existing fields
+			// according to VictoriaLogs data model.
+			// See https://docs.victoriametrics.com/victorialogs/keyconcepts/#stream-fields
+			continue
+		}
+
+		dst = quicktemplate.AppendJSONString(dst, name, true)
+		dst = append(dst, ':')
+		dst = quicktemplate.AppendJSONString(dst, value, true)
+		dst = append(dst, ',')
+	}
+	if len(dst)-dstLen == 1 {
+		// skip empty row.
+		return dst[:dstLen]
+	}
+
+	// Replace the trailing comma with closing braces
+	dst = append(dst[:len(dst)-1], "}\n"...)
+	return dst
 }
 
 // ProcessTenantIDsRequest processes /select/tenant_ids request.
@@ -1309,24 +1387,6 @@ func (sw *syncWriter) Write(p []byte) (int, error) {
 	n, err := sw.w.Write(p)
 	sw.mu.Unlock()
 	return n, err
-}
-
-type bufferedWriter struct {
-	buf []byte
-	sw  *syncWriter
-}
-
-func (bw *bufferedWriter) Write(p []byte) (int, error) {
-	bw.buf = append(bw.buf, p...)
-
-	// Do not send bw.buf to bw.sw here, since the data at bw.buf may be incomplete (it must end with '\n')
-
-	return len(p), nil
-}
-
-func (bw *bufferedWriter) FlushIgnoreErrors() {
-	_, _ = bw.sw.Write(bw.buf)
-	bw.buf = bw.buf[:0]
 }
 
 type commonArgs struct {

app/vlselect/logsql/query_response.qtpl
@@ -4,35 +4,6 @@
 
 {% stripspace %}
 
-// JSONRow creates JSON row from the given fields.
-{% func JSONRow(columns []logstorage.BlockColumn, rowIdx int) %}
-	{% code
-		i := 0
-		for i < len(columns) && columns[i].Values[rowIdx] == "" {
-			i++
-		}
-		columns = columns[i:]
-	%}
-	{% if len(columns) == 0 %}
-		{% return %}
-	{% endif %}
-	{
-	{% code	c := &columns[0] %}
-	{%q= c.Name %}:{%q= c.Values[rowIdx] %}
-	{% code columns = columns[1:] %}
-	{% for colIdx := range columns %}
-		{% code
-			c := &columns[colIdx]
-			v := c.Values[rowIdx]
-		%}
-		{% if v == "" %}
-			{% continue %}
-		{% endif %}
-		,{%q= c.Name %}:{%q= c.Values[rowIdx] %}
-	{% endfor %}
-	}{% newline %}
-{% endfunc %}
-
 // JSONRows prints formatted rows
 {% func JSONRows(rows [][]logstorage.Field) %}
 	{% if len(rows) == 0 %}
@@ -44,7 +15,6 @@
 			{% continue %}
 		{% endif %}
 		{
-		{% if len(fields) > 0 %}
 			{% code
 				f := fields[0]
 				fields = fields[1:]
@@ -56,7 +26,6 @@
 				{% endif %}
 				,{%q= f.Name %}:{%q= f.Value %}
 			{% endfor %}
-		{% endif %}
 		}{% newline %}
 	{% endfor %}
 {% endfunc %}

app/vlselect/logsql/query_response.qtpl.go
@@ -9,7 +9,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaLogs/lib/logstorage"
 )
 
-// JSONRow creates JSON row from the given fields.
+// JSONRows prints formatted rows
 
 //line app/vlselect/logsql/query_response.qtpl:8
 import (
@@ -25,177 +25,86 @@ var (
 )
 
 //line app/vlselect/logsql/query_response.qtpl:8
-func StreamJSONRow(qw422016 *qt422016.Writer, columns []logstorage.BlockColumn, rowIdx int) {
-//line app/vlselect/logsql/query_response.qtpl:10
-	i := 0
-	for i < len(columns) && columns[i].Values[rowIdx] == "" {
-		i++
-	}
-	columns = columns[i:]
-
-//line app/vlselect/logsql/query_response.qtpl:16
-	if len(columns) == 0 {
-//line app/vlselect/logsql/query_response.qtpl:17
-		return
-//line app/vlselect/logsql/query_response.qtpl:18
-	}
-//line app/vlselect/logsql/query_response.qtpl:18
-	qw422016.N().S(`{`)
-//line app/vlselect/logsql/query_response.qtpl:20
-	c := &columns[0]
-
-//line app/vlselect/logsql/query_response.qtpl:21
-	qw422016.N().Q(c.Name)
-//line app/vlselect/logsql/query_response.qtpl:21
-	qw422016.N().S(`:`)
-//line app/vlselect/logsql/query_response.qtpl:21
-	qw422016.N().Q(c.Values[rowIdx])
-//line app/vlselect/logsql/query_response.qtpl:22
-	columns = columns[1:]
-
-//line app/vlselect/logsql/query_response.qtpl:23
-	for colIdx := range columns {
-//line app/vlselect/logsql/query_response.qtpl:25
-		c := &columns[colIdx]
-		v := c.Values[rowIdx]
-
-//line app/vlselect/logsql/query_response.qtpl:28
-		if v == "" {
-//line app/vlselect/logsql/query_response.qtpl:29
-			continue
-//line app/vlselect/logsql/query_response.qtpl:30
-		}
-//line app/vlselect/logsql/query_response.qtpl:30
-		qw422016.N().S(`,`)
-//line app/vlselect/logsql/query_response.qtpl:31
-		qw422016.N().Q(c.Name)
-//line app/vlselect/logsql/query_response.qtpl:31
-		qw422016.N().S(`:`)
-//line app/vlselect/logsql/query_response.qtpl:31
-		qw422016.N().Q(c.Values[rowIdx])
-//line app/vlselect/logsql/query_response.qtpl:32
-	}
-//line app/vlselect/logsql/query_response.qtpl:32
-	qw422016.N().S(`}`)
-//line app/vlselect/logsql/query_response.qtpl:33
-	qw422016.N().S(`
-`)
-//line app/vlselect/logsql/query_response.qtpl:34
-}
-
-//line app/vlselect/logsql/query_response.qtpl:34
-func WriteJSONRow(qq422016 qtio422016.Writer, columns []logstorage.BlockColumn, rowIdx int) {
-//line app/vlselect/logsql/query_response.qtpl:34
-	qw422016 := qt422016.AcquireWriter(qq422016)
-//line app/vlselect/logsql/query_response.qtpl:34
-	StreamJSONRow(qw422016, columns, rowIdx)
-//line app/vlselect/logsql/query_response.qtpl:34
-	qt422016.ReleaseWriter(qw422016)
-//line app/vlselect/logsql/query_response.qtpl:34
-}
-
-//line app/vlselect/logsql/query_response.qtpl:34
-func JSONRow(columns []logstorage.BlockColumn, rowIdx int) string {
-//line app/vlselect/logsql/query_response.qtpl:34
-	qb422016 := qt422016.AcquireByteBuffer()
-//line app/vlselect/logsql/query_response.qtpl:34
-	WriteJSONRow(qb422016, columns, rowIdx)
-//line app/vlselect/logsql/query_response.qtpl:34
-	qs422016 := string(qb422016.B)
-//line app/vlselect/logsql/query_response.qtpl:34
-	qt422016.ReleaseByteBuffer(qb422016)
-//line app/vlselect/logsql/query_response.qtpl:34
-	return qs422016
-//line app/vlselect/logsql/query_response.qtpl:34
-}
-
-// JSONRows prints formatted rows
-
-//line app/vlselect/logsql/query_response.qtpl:37
 func StreamJSONRows(qw422016 *qt422016.Writer, rows [][]logstorage.Field) {
-//line app/vlselect/logsql/query_response.qtpl:38
+//line app/vlselect/logsql/query_response.qtpl:9
 	if len(rows) == 0 {
-//line app/vlselect/logsql/query_response.qtpl:39
+//line app/vlselect/logsql/query_response.qtpl:10
 		return
-//line app/vlselect/logsql/query_response.qtpl:40
+//line app/vlselect/logsql/query_response.qtpl:11
 	}
-//line app/vlselect/logsql/query_response.qtpl:41
+//line app/vlselect/logsql/query_response.qtpl:12
 	for _, fields := range rows {
-//line app/vlselect/logsql/query_response.qtpl:42
+//line app/vlselect/logsql/query_response.qtpl:13
 		fields = logstorage.SkipLeadingFieldsWithoutValues(fields)
 
-//line app/vlselect/logsql/query_response.qtpl:43
+//line app/vlselect/logsql/query_response.qtpl:14
 		if len(fields) == 0 {
-//line app/vlselect/logsql/query_response.qtpl:44
+//line app/vlselect/logsql/query_response.qtpl:15
 			continue
-//line app/vlselect/logsql/query_response.qtpl:45
+//line app/vlselect/logsql/query_response.qtpl:16
 		}
-//line app/vlselect/logsql/query_response.qtpl:45
+//line app/vlselect/logsql/query_response.qtpl:16
 		qw422016.N().S(`{`)
-//line app/vlselect/logsql/query_response.qtpl:47
-		if len(fields) > 0 {
-//line app/vlselect/logsql/query_response.qtpl:49
-			f := fields[0]
-			fields = fields[1:]
+//line app/vlselect/logsql/query_response.qtpl:19
+		f := fields[0]
+		fields = fields[1:]
 
-//line app/vlselect/logsql/query_response.qtpl:52
-			qw422016.N().Q(f.Name)
-//line app/vlselect/logsql/query_response.qtpl:52
-			qw422016.N().S(`:`)
-//line app/vlselect/logsql/query_response.qtpl:52
-			qw422016.N().Q(f.Value)
-//line app/vlselect/logsql/query_response.qtpl:53
-			for _, f := range fields {
-//line app/vlselect/logsql/query_response.qtpl:54
-				if f.Value == "" {
-//line app/vlselect/logsql/query_response.qtpl:55
-					continue
-//line app/vlselect/logsql/query_response.qtpl:56
-				}
-//line app/vlselect/logsql/query_response.qtpl:56
-				qw422016.N().S(`,`)
-//line app/vlselect/logsql/query_response.qtpl:57
-				qw422016.N().Q(f.Name)
-//line app/vlselect/logsql/query_response.qtpl:57
-				qw422016.N().S(`:`)
-//line app/vlselect/logsql/query_response.qtpl:57
-				qw422016.N().Q(f.Value)
-//line app/vlselect/logsql/query_response.qtpl:58
+//line app/vlselect/logsql/query_response.qtpl:22
+		qw422016.N().Q(f.Name)
+//line app/vlselect/logsql/query_response.qtpl:22
+		qw422016.N().S(`:`)
+//line app/vlselect/logsql/query_response.qtpl:22
+		qw422016.N().Q(f.Value)
+//line app/vlselect/logsql/query_response.qtpl:23
+		for _, f := range fields {
+//line app/vlselect/logsql/query_response.qtpl:24
+			if f.Value == "" {
+//line app/vlselect/logsql/query_response.qtpl:25
+				continue
+//line app/vlselect/logsql/query_response.qtpl:26
 			}
-//line app/vlselect/logsql/query_response.qtpl:59
+//line app/vlselect/logsql/query_response.qtpl:26
+			qw422016.N().S(`,`)
+//line app/vlselect/logsql/query_response.qtpl:27
+			qw422016.N().Q(f.Name)
+//line app/vlselect/logsql/query_response.qtpl:27
+			qw422016.N().S(`:`)
+//line app/vlselect/logsql/query_response.qtpl:27
+			qw422016.N().Q(f.Value)
+//line app/vlselect/logsql/query_response.qtpl:28
 		}
-//line app/vlselect/logsql/query_response.qtpl:59
+//line app/vlselect/logsql/query_response.qtpl:28
 		qw422016.N().S(`}`)
-//line app/vlselect/logsql/query_response.qtpl:60
+//line app/vlselect/logsql/query_response.qtpl:29
 		qw422016.N().S(`
 `)
-//line app/vlselect/logsql/query_response.qtpl:61
+//line app/vlselect/logsql/query_response.qtpl:30
 	}
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 }
 
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 func WriteJSONRows(qq422016 qtio422016.Writer, rows [][]logstorage.Field) {
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	StreamJSONRows(qw422016, rows)
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	qt422016.ReleaseWriter(qw422016)
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 }
 
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 func JSONRows(rows [][]logstorage.Field) string {
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	qb422016 := qt422016.AcquireByteBuffer()
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	WriteJSONRows(qb422016, rows)
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	qs422016 := string(qb422016.B)
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	qt422016.ReleaseByteBuffer(qb422016)
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 	return qs422016
-//line app/vlselect/logsql/query_response.qtpl:62
+//line app/vlselect/logsql/query_response.qtpl:31
 }

apptest/client.go
@@ -32,36 +32,31 @@ func (c *Client) CloseConnections() {
 	c.httpCli.CloseIdleConnections()
 }
 
-// Get sends a HTTP GET request, returns
-// the response body and status code to the caller.
+// Get sends a HTTP GET request, returns the response body and status code to the caller.
 func (c *Client) Get(t *testing.T, url string) (string, int) {
 	t.Helper()
 	return c.do(t, http.MethodGet, url, "", nil)
 }
 
-// Post sends a HTTP POST request, returns
-// the response body and status code to the caller.
+// Post sends a HTTP POST request, returns the response body and status code to the caller.
 func (c *Client) Post(t *testing.T, url, contentType string, data []byte) (string, int) {
 	t.Helper()
 	return c.do(t, http.MethodPost, url, contentType, data)
 }
 
-// PostForm sends a HTTP POST request containing the POST-form data, returns
-// the response body and status code to the caller.
+// PostForm sends a HTTP POST request containing the POST-form data, returns the response body and status code to the caller.
 func (c *Client) PostForm(t *testing.T, url string, data url.Values) (string, int) {
 	t.Helper()
 	return c.Post(t, url, "application/x-www-form-urlencoded", []byte(data.Encode()))
 }
 
-// Delete sends a HTTP DELETE request and returns the response body and status code
-// to the caller.
+// Delete sends a HTTP DELETE request and returns the response body and status code to the caller.
 func (c *Client) Delete(t *testing.T, url string) (string, int) {
 	t.Helper()
 	return c.do(t, http.MethodDelete, url, "", nil)
 }
 
-// do prepares a HTTP request, sends it to the server, receives the response
-// from the server, returns the response body and status code to the caller.
+// do prepares a HTTP request, sends it to the server, receives the response from the server, returns the response body and status code to the caller.
 func (c *Client) do(t *testing.T, method, url, contentType string, data []byte) (string, int) {
 	t.Helper()
 

apptest/model.go
@@ -18,6 +18,7 @@ type QueryOpts struct {
 	End          string
 	Limit        string
 	ExtraFilters []string
+	Format       string
 }
 
 func (qos *QueryOpts) asURLValues() url.Values {
@@ -27,6 +28,7 @@ func (qos *QueryOpts) asURLValues() url.Values {
 	addNonEmpty(uv, "end", qos.End)
 	addNonEmpty(uv, "limit", qos.Limit)
 	addNonEmpty(uv, "extra_filters", qos.ExtraFilters...)
+	addNonEmpty(uv, "format", qos.Format)
 	return uv
 }
 

commit 6461fdfc691e80a89d9755cb65a85f7d71346cf6

app/vlstorage: clone _time field values
This commit fixes a bug in buffer reuse that could cause _time field to become invalid.
The bug was introduced in 885f548f3f8de4bc5dc20c8e188b5a908bcf5d81

app/vlstorage/lastnoptimization.go
@@ -185,7 +185,7 @@ func getLogRowsFromDataBlock(db *logstorage.DataBlock) ([]logRow, error) {
 		// The _time column must go first, since the query results are sorted by _time.
 		fieldsBuf = append(fieldsBuf, logstorage.Field{
 			Name:  "_time",
-			Value: timestampsColumn.Values[i],
+			Value: strings.Clone(timestampsColumn.Values[i]),
 		})
 
 		for j, c := range columns {