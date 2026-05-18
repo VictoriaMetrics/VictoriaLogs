69980e7f98a7f723e6711a84b49ad15c88af4efc
Cuong Le <cuongleqq@gmail.com>
2026-05-18 22:07:08 +0700

    lib/logstorage: fix unroll and json_array_len pipes ignoring JSON arrays preceded by whitespace

--- lib/logstorage/pipe_json_array_len_test.go
+++ lib/logstorage/pipe_json_array_len_test.go
@@ -43,6 +43,9 @@ func TestPipeJSONArrayLen(t *testing.T) {
 			{"baz", "1234567890"},
 		},
 		{
+			{"foo", `  ["a","b"]`},
+		},
+		{
 			{"foo", `abc`},
 			{"bar", `de`},
 		},
@@ -54,6 +57,10 @@ func TestPipeJSONArrayLen(t *testing.T) {
 			{"foo", `["abcde",2,{"bar":"x,y","z":[1,2]}]`},
 			{"baz", "1234567890"},
 			{"x", "3"},
+		},
+		{
+			{"foo", `  ["a","b"]`},
+			{"x", "2"},
 		},
 		{
 			{"foo", `abc`},
