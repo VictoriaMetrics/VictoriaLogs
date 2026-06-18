VictoriaMetrics/VictoriaLogs

commit c330719f6793faba1988ae9a482aff8e9162cdc9

lib/logstorage: disallow `filter` pipes without `filter` prefix if they are hard to distiguinsh from other pipes
Updates https://github.com/VictoriaMetrics/VictoriaLogs/issues/1454

lib/logstorage/pipe.go
@@ -146,7 +146,7 @@ func parsePipe(lex *lexer) (pipe, error) {
 		return p, nil
 	}
 
-	if isStatsFuncName(lex.rawToken) || lex.isKeyword("by", "(") {
+	if isLikelyStatsPipe(lex) {
 		// Try parsing stats pipe without 'stats' keyword
 		ps, err := parsePipeStatsNoStatsKeyword(lex)
 		if err != nil {
@@ -155,15 +155,38 @@ func parsePipe(lex *lexer) (pipe, error) {
 		return ps, nil
 	}
 
-	// Try parsing filter pipe without 'filter' keyword
-	lexState := lex.backupState()
-	pf, err := parsePipeFilterNoFilterKeyword(lex)
-	if err == nil {
+	if isLikelyFilterPipe(lex) {
+		// Try parsing filter pipe without 'filter' keyword
+		pf, err := parsePipeFilterNoFilterKeyword(lex)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse 'filter' pipe: %w", err)
+		}
 		return pf, nil
 	}
-	lex.restoreState(lexState)
 
 	return nil, fmt.Errorf("unexpected pipe %q", lex.token)
+}
+
+func isLikelyStatsPipe(lex *lexer) bool {
+	return isStatsFuncName(lex.rawToken) || lex.isKeyword("by", "(")
+}
+
+func isLikelyFilterPipe(lex *lexer) bool {
+	if lex.isQuotedToken() {
+		return true
+	}
+	if lex.isKeyword("*", "-", "~") {
+		return true
+	}
+
+	lexState := lex.backupState()
+	defer lex.restoreState(lexState)
+
+	stopTokens := []string{":"}
+	if _, err := lex.nextCompoundTokenExt(stopTokens); err != nil {
+		return false
+	}
+	return lex.isKeyword(":")
 }
 
 var pipeParsers map[string]pipeParseFunc

lib/logstorage/parser_test.go
@@ -2434,7 +2434,10 @@ func TestParseQuery_Success(t *testing.T) {
 	// filter pipe
 	f(`* | filter error ip:12.3.4.5 or warn`, `error ip:12.3.4.5 or warn`)
 	f(`foo | stats by (host) count() logs | filter logs:>50 | sort by (logs desc) | limit 10`, `foo | stats by (host) count(*) as logs | filter logs:>50 | sort by (logs desc) limit 10`)
-	f(`* | error`, `error`)
+	f(`* | "error"`, `error`)
+	f(`* | filter error`, `error`)
+	f(`* | -unpack_logfmt`, `!"unpack_logfmt"`)
+	f(`* |~foo`, `~foo`)
 	f(`* | "by"`, `"by"`)
 	f(`* | "stats" *`, `"stats"`)
 	f(`* | * "count"`, `"count"`)
@@ -2543,7 +2546,7 @@ func TestParseQuery_Success(t *testing.T) {
 	f(`not(a)`, `!a`)
 	f(`a and(b)`, `a b`)
 	f(`a or(b)`, `a or b`)
-	f(`a|foobar`, `a foobar`)
+	f(`a|foo:bar`, `a foo:bar`)
 	f(`a|~b`, `a ~b`)
 	f(`a:~b`, `a:~b`)
 	f(`(~a)`, `~a`)
@@ -3506,6 +3509,11 @@ func TestParseQuery_Failure(t *testing.T) {
 	f(`* | (x)`)
 	f(`* | by (x)`)
 	f(`* | (x) count`)
+
+	// incorrect pipes
+	f(`* | unpack_word`)
+	f(`* | coalsce(a,b)`)
+	f(`* | unique by (_msg) limit 1`)
 }
 
 func TestQueryGetNeededColumns(t *testing.T) {
@@ -4608,13 +4616,13 @@ func TestQueryHasGlobalTimeFilter(t *testing.T) {
 	f(`* | count()`, false)
 	f(`error OR _time:5m | count()`, false)
 	f(`(_time:5m AND error) OR (_time:5m AND warn) | count()`, false)
-	f(`* | error OR _time:5m | count()`, false)
+	f(`* | "error" OR _time:5m | count()`, false)
 
 	f(`_time:5m | count()`, true)
 	f(`_time:2023-04-25T22:45:59Z | count()`, true)
 	f(`error AND _time:5m | count()`, true)
 	f(`error AND (_time:5m AND warn) | count()`, true)
-	f(`* | error AND _time:5m | count()`, true)
+	f(`* | "error" AND _time:5m | count()`, true)
 }
 
 func TestQuery_AddExtraFilters(t *testing.T) {

lib/logstorage/pipe_collapse_nums_test.go
@@ -137,7 +137,7 @@ func TestPipeCollapseNumsUpdateNeededFields(t *testing.T) {
 
 	// all the needed fields
 	f(`collapse_nums`, "*", "", "*", "")
-	f(`collaps_nums if (f1:q) at x`, "*", "", "*", "")
+	f(`collapse_nums if (f1:q) at x`, "*", "", "*", "")
 
 	// unneeded fields do not intersect with at field
 	f(`collapse_nums at x`, "*", "f1,f2", "*", "f1,f2")

lib/logstorage/pipe_filter_test.go
@@ -33,7 +33,7 @@ func TestPipeFilter(t *testing.T) {
 	}
 
 	// filter mismatch, missing 'filter' prefix
-	f("abc", [][]Field{
+	f(`"abc"`, [][]Field{
 		{
 			{"_msg", `{"foo":"bar"}`},
 			{"a", `test`},
@@ -49,7 +49,7 @@ func TestPipeFilter(t *testing.T) {
 	}, [][]Field{})
 
 	// filter match, missing 'filter' prefix
-	f("foo", [][]Field{
+	f(`"foo"`, [][]Field{
 		{
 			{"_msg", `{"foo":"bar"}`},
 			{"a", `test`},

commit 920f4a4f4bdb54e6c56784ac86a206ba03d8190c

lib/logstorage: fix filter pipe parsing without the filter prefix
This fixes the bug that was introduced in c330719f6793faba1988ae9a482aff8e9162cdc9,
where valid queries like `... | !foo` became invalid.

Updates #1522
Updates #1454

lib/logstorage/if_filter.go
@@ -30,7 +30,7 @@ func parseIfFilter(lex *lexer) (*ifFilter, error) {
 		return newIfFilter(newFilterNoop()), nil
 	}
 
-	f, err := parseFilter(lex, true)
+	f, err := parseFilter(lex, false)
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse 'if' filter: %w", err)
 	}

lib/logstorage/parser.go
@@ -1928,7 +1928,7 @@ func parseQuery(lex *lexer) (*Query, error) {
 	lex.pushQueryOptions(&q.opts)
 	defer lex.popQueryOptions()
 
-	f, err := parseFilter(lex, true)
+	f, err := parseFilter(lex, false)
 	if err != nil {
 		return nil, fmt.Errorf("%w; context: [%s]", err, lex.context())
 	}
@@ -2113,12 +2113,12 @@ func parseQueryOptions(dstOpts *queryOptions, lex *lexer) error {
 	}
 }
 
-func parseFilter(lex *lexer, allowPipeKeywords bool) (filter, error) {
+func parseFilter(lex *lexer, strictPhrases bool) (filter, error) {
 	if lex.isQueryPartTrailer() {
 		return nil, fmt.Errorf("missing query")
 	}
 
-	if !allowPipeKeywords {
+	if strictPhrases {
 		// Verify the first token in the filter doesn't match pipe names.
 		firstToken := strings.ToLower(lex.rawToken)
 		if firstToken == "by" || isPipeName(firstToken) || isStatsFuncName(firstToken) {
@@ -2127,17 +2127,17 @@ func parseFilter(lex *lexer, allowPipeKeywords bool) (filter, error) {
 		}
 	}
 
-	fo, err := parseFilterOr(lex, "")
+	fo, err := parseFilterOr(lex, "", strictPhrases)
 	if err != nil {
 		return nil, err
 	}
 	return fo, nil
 }
 
-func parseFilterOr(lex *lexer, fieldName string) (filter, error) {
+func parseFilterOr(lex *lexer, fieldName string, strictPhrases bool) (filter, error) {
 	var filters []filter
 	for {
-		f, err := parseFilterAnd(lex, fieldName)
+		f, err := parseFilterAnd(lex, fieldName, strictPhrases)
 		if err != nil {
 			return nil, err
 		}
@@ -2155,10 +2155,10 @@ func parseFilterOr(lex *lexer, fieldName string) (filter, error) {
 	}
 }
 
-func parseFilterAnd(lex *lexer, fieldName string) (filter, error) {
+func parseFilterAnd(lex *lexer, fieldName string, strictPhrases bool) (filter, error) {
 	var filters []filter
 	for {
-		f, err := parseFilterGeneric(lex, fieldName)
+		f, err := parseFilterGeneric(lex, fieldName, strictPhrases)
 		if err != nil {
 			return nil, err
 		}
@@ -2176,7 +2176,7 @@ func parseFilterAnd(lex *lexer, fieldName string) (filter, error) {
 	}
 }
 
-func parseFilterGeneric(lex *lexer, fieldName string) (filter, error) {
+func parseFilterGeneric(lex *lexer, fieldName string, strictPhrases bool) (filter, error) {
 	if lex.isKeyword("") {
 		return nil, fmt.Errorf("unexpected end of query after %q; expecting a filter", lex.prevRawToken)
 	}
@@ -2267,15 +2267,16 @@ func parseFilterGeneric(lex *lexer, fieldName string) (filter, error) {
 	case lex.isKeyword("_stream"):
 		return parseFilterStream(lex, fieldName)
 	default:
-		return parseFilterPhrase(lex, fieldName)
+		return parseFilterPhrase(lex, fieldName, strictPhrases)
 	}
 }
 
-func parseFilterPhrase(lex *lexer, fieldName string) (filter, error) {
+func parseFilterPhrase(lex *lexer, fieldName string, strictPhrases bool) (filter, error) {
 	var stopTokens []string
 	if fieldName == "" {
 		stopTokens = []string{":"}
 	}
+	phraseIsQuoted := lex.isQuotedToken()
 	phrase, err := lex.nextCompoundTokenExt(stopTokens)
 	if err != nil {
 		return nil, err
@@ -2285,7 +2286,10 @@ func parseFilterPhrase(lex *lexer, fieldName string) (filter, error) {
 		lex.nextToken()
 		if fieldName == "" && lex.isKeyword(":") {
 			lex.nextToken()
-			return parseFilterGeneric(lex, phrase+"*")
+			return parseFilterGeneric(lex, phrase+"*", false)
+		}
+		if fieldName == "" && !phraseIsQuoted && strictPhrases {
+			return nil, fmt.Errorf("search by prefix %q requires a field name; use %q* or field:%q*", phrase, phrase, phrase)
 		}
 		return newFilterPrefix(fieldName, phrase), nil
 	}
@@ -2302,10 +2306,12 @@ func parseFilterPhrase(lex *lexer, fieldName string) (filter, error) {
 		case "_stream":
 			return parseFilterStreamInternal(lex, "_stream")
 		default:
-			return parseFilterGeneric(lex, phrase)
+			return parseFilterGeneric(lex, phrase, false)
 		}
 	}
-
+	if fieldName == "" && !phraseIsQuoted && strictPhrases {
+		return nil, fmt.Errorf("search phrase %q requires a field name; use %q or field:%s", phrase, phrase, phrase)
+	}
 	// The phrase is a search phrase.
 	return newFilterPhrase(fieldName, phrase), nil
 }
@@ -2313,7 +2319,7 @@ func parseFilterPhrase(lex *lexer, fieldName string) (filter, error) {
 func parseFilterParens(lex *lexer, fieldName string) (filter, error) {
 	lex.nextToken()
 
-	f, err := parseFilterOr(lex, fieldName)
+	f, err := parseFilterOr(lex, fieldName, false)
 	if err != nil {
 		return nil, err
 	}
@@ -2329,7 +2335,7 @@ func parseFilterParens(lex *lexer, fieldName string) (filter, error) {
 func parseFilterNot(lex *lexer, fieldName string) (filter, error) {
 	lex.nextToken()
 
-	f, err := parseFilterGeneric(lex, fieldName)
+	f, err := parseFilterGeneric(lex, fieldName, false)
 	if err != nil {
 		return nil, err
 	}
@@ -2360,7 +2366,7 @@ func parseFuncArgMaybePrefix(lex *lexer, fieldName string, callback func(arg str
 
 	if !lex.isKeyword("(") {
 		lex.restoreState(lexState)
-		return parseFilterPhrase(lex, fieldName)
+		return parseFilterPhrase(lex, fieldName, false)
 	}
 	lex.nextToken()
 
@@ -2710,7 +2716,7 @@ func parseFilterStar(lex *lexer, fieldName string) (filter, error) {
 	if fieldName == "" && lex.isKeyword(":") {
 		// The '*:something' filter
 		lex.nextToken()
-		return parseFilterGeneric(lex, "*")
+		return parseFilterGeneric(lex, "*", false)
 	}
 
 	if lex.isSkippedSpace || lex.isQueryPartTrailer() {
@@ -2900,7 +2906,7 @@ func parseFilterRange(lex *lexer, fieldName string) (filter, error) {
 		includeMinValue = true
 	default:
 		lex.restoreState(lexState)
-		return parseFilterPhrase(lex, fieldName)
+		return parseFilterPhrase(lex, fieldName, false)
 	}
 	lex.nextToken()
 
@@ -2980,7 +2986,7 @@ func parseFuncArgs(lex *lexer, fieldName string, callback func(funcName string, 
 
 	if !lex.isKeyword("(") {
 		lex.restoreState(lexState)
-		return parseFilterPhrase(lex, fieldName)
+		return parseFilterPhrase(lex, fieldName, false)
 	}
 
 	args, err := parseArgsInParens(lex)
@@ -3106,7 +3112,7 @@ func startsWithYear(s string) bool {
 
 func parseFilterTimeGeneric(lex *lexer, fieldName string) (filter, error) {
 	if fieldName != "" {
-		return parseFilterPhrase(lex, fieldName)
+		return parseFilterPhrase(lex, fieldName, false)
 	}
 
 	lexState := lex.backupState()
@@ -3114,7 +3120,7 @@ func parseFilterTimeGeneric(lex *lexer, fieldName string) (filter, error) {
 
 	if !lex.isKeyword(":") {
 		lex.restoreState(lexState)
-		return parseFilterPhrase(lex, "")
+		return parseFilterPhrase(lex, "", false)
 	}
 	lex.nextToken()
 
@@ -3664,7 +3670,7 @@ func stripTimezoneSuffix(s string) string {
 
 func parseFilterStreamID(lex *lexer, fieldName string) (filter, error) {
 	if fieldName != "" {
-		return parseFilterPhrase(lex, fieldName)
+		return parseFilterPhrase(lex, fieldName, false)
 	}
 
 	lexState := lex.backupState()
@@ -3672,7 +3678,7 @@ func parseFilterStreamID(lex *lexer, fieldName string) (filter, error) {
 
 	if !lex.isKeyword(":") {
 		lex.restoreState(lexState)
-		return parseFilterPhrase(lex, "")
+		return parseFilterPhrase(lex, "", false)
 	}
 	lex.nextToken()
 
@@ -3802,14 +3808,14 @@ func parseStreamID(lex *lexer) (streamID, error) {
 
 func parseFilterStream(lex *lexer, fieldName string) (filter, error) {
 	if fieldName != "" {
-		return parseFilterPhrase(lex, fieldName)
+		return parseFilterPhrase(lex, fieldName, false)
 	}
 	lexState := lex.backupState()
 	lex.nextToken()
 
 	if !lex.isKeyword(":") {
 		lex.restoreState(lexState)
-		return parseFilterPhrase(lex, "")
+		return parseFilterPhrase(lex, "", false)
 	}
 	lex.nextToken()
 

lib/logstorage/pipe.go
@@ -156,14 +156,11 @@ func parsePipe(lex *lexer) (pipe, error) {
 	}
 
 	// Try parsing filter pipe without 'filter' keyword
-	lexState := lex.backupState()
 	pf, err := parsePipeFilterNoFilterKeyword(lex)
-	if err == nil {
-		return pf, nil
+	if err != nil {
+		return nil, err
 	}
-	lex.restoreState(lexState)
-
-	return nil, fmt.Errorf("unexpected pipe %q", lex.token)
+	return pf, nil
 }
 
 var pipeParsers map[string]pipeParseFunc

lib/logstorage/pipe_filter.go
@@ -109,22 +109,19 @@ func (pfp *pipeFilterProcessor) flush() error {
 }
 
 func parsePipeFilter(lex *lexer) (pipe, error) {
-	return parsePipeFilterExt(lex, true)
-}
-
-func parsePipeFilterNoFilterKeyword(lex *lexer) (pipe, error) {
+	if !lex.isKeyword("filter", "where") {
+		return nil, fmt.Errorf("expecting 'filter' or 'where'; got %q", lex.token)
+	}
+	lex.nextToken()
 	return parsePipeFilterExt(lex, false)
 }
 
-func parsePipeFilterExt(lex *lexer, needFilterKeyword bool) (pipe, error) {
-	if needFilterKeyword {
-		if !lex.isKeyword("filter", "where") {
-			return nil, fmt.Errorf("expecting 'filter' or 'where'; got %q", lex.token)
-		}
-		lex.nextToken()
-	}
+func parsePipeFilterNoFilterKeyword(lex *lexer) (pipe, error) {
+	return parsePipeFilterExt(lex, true)
+}
 
-	f, err := parseFilter(lex, needFilterKeyword)
+func parsePipeFilterExt(lex *lexer, strictPhrases bool) (pipe, error) {
+	f, err := parseFilter(lex, strictPhrases)
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse 'filter': %w", err)
 	}

lib/logstorage/parser_test.go
@@ -2434,7 +2434,10 @@ func TestParseQuery_Success(t *testing.T) {
 	// filter pipe
 	f(`* | filter error ip:12.3.4.5 or warn`, `error ip:"12.3.4.5" or warn`)
 	f(`foo | stats by (host) count() logs | filter logs:>50 | sort by (logs desc) | limit 10`, `foo | stats by (host) count(*) as logs | filter logs:>50 | sort by (logs desc) limit 10`)
-	f(`* | error`, `error`)
+	f(`* | "error"`, `error`)
+	f(`* | filter error`, `error`)
+	f(`* | -unpack_logfmt`, `!"unpack_logfmt"`)
+	f(`* |~foo`, `~foo`)
 	f(`* | "by"`, `"by"`)
 	f(`* | "stats" *`, `"stats"`)
 	f(`* | * "count"`, `"count"`)
@@ -2543,7 +2546,7 @@ func TestParseQuery_Success(t *testing.T) {
 	f(`not(a)`, `!a`)
 	f(`a and(b)`, `a b`)
 	f(`a or(b)`, `a or b`)
-	f(`a|foobar`, `a foobar`)
+	f(`a|foo:bar`, `a foo:bar`)
 	f(`a|~b`, `a ~b`)
 	f(`a:~b`, `a:~b`)
 	f(`(~a)`, `~a`)
@@ -2564,7 +2567,7 @@ func TestParseQuery_Success(t *testing.T) {
 	f(`foo | filter fields`, `foo "fields"`)
 	f(`foo | filter by`, `foo "by"`)
 	f(`foo | filter count`, `foo "count"`)
-	f(`foo | * bar`, `foo bar`)
+	f(`foo | * "bar"`, `foo bar`)
 	f(`foo | -bar`, `foo !bar`)
 
 	// wildcard field names in filters
@@ -2682,6 +2685,27 @@ func TestParseQuery_Success(t *testing.T) {
 
 	f(`a:in(x | keep a;);`, `a:in(x | fields a)`)
 	f(`a:in(x | keep a;) | stats count() if (x;) y;`, `a:in(x | fields a) | stats count(*) if (x) as y`)
+
+	// edge cases from https://github.com/VictoriaMetrics/VictoriaLogs/issues/1454
+	f(`* | not (field:="value")`, `!field:=value`)
+	f(`* | !(field:="values")`, `!field:="values"`)
+	f(`* | ~"foo"`, `~foo`)
+	f(`* | !"foo"`, `!foo`)
+	f(`* | !~"foo"`, `!~foo`)
+	f(`* | !="foo"`, `!=foo`)
+	f(`* | ="foo"`, `=foo`)
+	f(`* | >"foo"`, `>foo`)
+	f(`* | <"foo"`, `<foo`)
+	f(`* | {foo="bar"}`, `{foo="bar"}`)
+	f(`* | not (rule:="Run shell untrusted" output_fields.proc.pname:="postgres")`, `!(rule:="Run shell untrusted" "output_fields.proc.pname":=postgres)`)
+
+	// strict filter parsing without the `filter` keyword
+	f(`foo | "bar"`, `foo bar`)
+	f(`foo | _msg:bar`, `foo bar`)
+	f(`* | "":"foo" "":bar`, `foo bar`)
+	f(`* | "foo" "bar"`, `foo bar`)
+	f(`* | "foo" and "bar"`, `foo bar`)
+	f(`* | "or" (foo bar)`, `"or" foo bar`)
 }
 
 func TestParseQuery_Failure(t *testing.T) {
@@ -3507,6 +3531,12 @@ func TestParseQuery_Failure(t *testing.T) {
 	f(`* | (x)`)
 	f(`* | by (x)`)
 	f(`* | (x) count`)
+
+	// incorrect pipes
+	f(`* | unpack_word`)
+	f(`* | coalsce(a,b)`)
+	f(`* | unique by (_msg) limit 1`)
+	f(`* | "foo" bar`)
 }
 
 func TestQueryGetNeededColumns(t *testing.T) {
@@ -4616,13 +4646,13 @@ func TestQueryHasGlobalTimeFilter(t *testing.T) {
 	f(`* | count()`, false)
 	f(`error OR _time:5m | count()`, false)
 	f(`(_time:5m AND error) OR (_time:5m AND warn) | count()`, false)
-	f(`* | error OR _time:5m | count()`, false)
+	f(`* | "error" OR _time:5m | count()`, false)
 
 	f(`_time:5m | count()`, true)
 	f(`_time:2023-04-25T22:45:59Z | count()`, true)
 	f(`error AND _time:5m | count()`, true)
 	f(`error AND (_time:5m AND warn) | count()`, true)
-	f(`* | error AND _time:5m | count()`, true)
+	f(`* | "error" AND _time:5m | count()`, true)
 }
 
 func TestQuery_AddExtraFilters(t *testing.T) {

lib/logstorage/pipe_collapse_nums_test.go
@@ -137,7 +137,7 @@ func TestPipeCollapseNumsUpdateNeededFields(t *testing.T) {
 
 	// all the needed fields
 	f(`collapse_nums`, "*", "", "*", "")
-	f(`collaps_nums if (f1:q) at x`, "*", "", "*", "")
+	f(`collapse_nums if (f1:q) at x`, "*", "", "*", "")
 
 	// unneeded fields do not intersect with at field
 	f(`collapse_nums at x`, "*", "f1,f2", "*", "f1,f2")

lib/logstorage/pipe_filter_test.go
@@ -33,7 +33,7 @@ func TestPipeFilter(t *testing.T) {
 	}
 
 	// filter mismatch, missing 'filter' prefix
-	f("abc", [][]Field{
+	f(`"abc"`, [][]Field{
 		{
 			{"_msg", `{"foo":"bar"}`},
 			{"a", `test`},
@@ -49,7 +49,7 @@ func TestPipeFilter(t *testing.T) {
 	}, [][]Field{})
 
 	// filter match, missing 'filter' prefix
-	f("foo", [][]Field{
+	f(`"foo"`, [][]Field{
 		{
 			{"_msg", `{"foo":"bar"}`},
 			{"a", `test`},